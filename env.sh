ba345693ec9e9f2177798d860548da7632318898
Benoit Gloria <gloria_b@epita.fr>
2020-01-29 17:12:20 +0100

    remove env.sh

--- env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#BASEDIR=$(dirname "$0")
-#BASEDIR=/home/sayrus/opt/GNAT/
-#echo $0
-#echo $BASEDIR
-#echo $PWD
-#export PATH=$BASEDIR/2019/bin/:$PATH
-#export PATH=$BASEDIR/2019-arm-elf/bin/:$PATH
-export GPR_PROJECT_PATH=/home/sayrus/Workspace/GISTRE/ADA/Gamepad/Ada_Drivers_Library/
