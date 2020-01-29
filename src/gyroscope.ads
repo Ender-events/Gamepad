2f55fae27f4a1aa5bfd18e5eccbbae8f342ee4a2
Tom Decrette <tom.decrette@epita.fr>
2020-01-29 23:50:32 +0100

    refacto: Fix Gyroscope style

--- src/gyroscope.ads
+++ src/gyroscope.ads
@@ -1,7 +1,6 @@
 with L3GD20;            use L3GD20;
-package gyroscope is
 
-
+package Gyroscope is
    type Angle is new Long_Integer;
 
    type Angles is tagged record
@@ -10,7 +9,14 @@ package gyroscope is
       Z : Angle;  -- yaw
    end record;
 
-   procedure Add_Equal(This : in out Angles; value : Angles);
+   procedure Add_Angle (This : in out Angles;
+                        Value : Angles);
+
+   procedure Div_Factor (This : in out Angles;
+                         Factor : Angle);
+
+   function Update_Gyro (Dur: Duration) return Angles;
+
 
    type AnglesDegree is record
       X : Float;  -- pitch, per Figure 2, pg 7 of the Datasheet
@@ -18,8 +24,11 @@ package gyroscope is
       Z : Float;  -- yaw
    end record;
 
+   function Average_Gyro return Angles;
    procedure Configure_Gyro;
    procedure Reset_Gyro;
-   function Update_Gyro (dt: Duration) return Angles;
 
-end gyroscope;
+   Axes        : Angles       := (0, 0, 0);
+   Axes_Offset : AnglesDegree := (0.0, 0.0, 0.0);
+   Axes_HP     : AnglesDegree := (0.0, 0.0, 0.0);
+end Gyroscope;
